55b986c25c8262943ea3a338e48185de8e51d337
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-01-27 10:01:03 +0000

    Modified JS. Muted db connection error

--- vc-admin-interface/server.js
+++ vc-admin-interface/server.js
@@ -5,7 +5,20 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 // MongoDB
-mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/products-demo');
+//mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/products-demo');
+
+
+/*
+************************************************************************
+*************************************************************************
+Mongoose: mpromise (mongoose's default promise library) is deprecated,
+ plug in your own promise library instead: http://mongoosejs.com/docs/promises.html
+
+************************************************************************
+**********************************************************************
+
+
+*/
 // mongoose.connection.on('error', function(){});
 
 // Express. Instantiate server
@@ -20,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', require('./routes/api'));
 
 // Start server
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8050
 , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 app.listen(port, ip, function() {
   console.log('Express server listening on %d', port);
